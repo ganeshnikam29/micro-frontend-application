346e5c0b48901fcb5686e59689dc4280b18e7584
Ganesh Nikam <ganeshni@synerzipune.local>
2023-05-31 16:14:51 +0530

    Changed Text

--- counterMFE/src/App.js
+++ counterMFE/src/App.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const App = () => {
     return (
-        <h1>This is React App. It is MFE-two</h1>
+        <h1>This is React App. It is MFE-two !</h1>
     )
 }
