edb9e5783158099d7a3e446a71d3085590833477
Ganesh Nikam <ganeshni@synerzipune.local>
2023-05-31 14:30:58 +0530

    Changed Text

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MyVueApp from "./components/MyVueApp";
 
 export const App = () => (
   <div style={{ background: "lightgrey" }}>
-    <h1>This is the Host!</h1>
+    <h1>This is Container App . It is Host!</h1>
     <h2>Remote App:</h2>
     <MyCounterApp />
     <MyVueApp />
